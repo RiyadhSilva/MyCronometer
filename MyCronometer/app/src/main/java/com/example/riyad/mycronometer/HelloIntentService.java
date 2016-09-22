cc9b99a4e6e38e88a4d1f219021c175c11259e76
RiyadhSilva <riyadhlevi@gmail.com>
2016-09-22 14:17:23 -0300

    Implementação - HelloService
    Implementação de uma classe que estende da IntentService....

---
+++ MyCronometer/app/src/main/java/com/example/riyad/mycronometer/HelloIntentService.java
@@ -0,0 +1,49 @@
+package com.example.riyad.mycronometer;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by riyad on 22/09/2016.
+ */
+public class HelloIntentService extends IntentService {
+    public HelloIntentService(){
+        super("NomeDaThreadAqui");
+    }
+
+    private static final int MAX = 10;
+    private static final String TAG = "livro";
+    private boolean running;
+
+    @Override
+    protected void onHandleIntent(Intent intent){
+        running = true;
+        //Este metodo executa em uma thread
+        //Quando ele terminar, o metodo stopSelf() sera chamado automaticamente
+        int count = 0;
+        while (running && count < MAX){
+            fazAlgumaCoisa();
+            Log.d(TAG, "ExemploServico executando..." + count);
+            count++;
+        }
+        Log.d(TAG, "ExemploServico fim.");
+    }
+
+    private void fazAlgumaCoisa(){
+        try{
+            //Simula algum processamento
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            Log.e(TAG, e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        //Ao encerrar o servico, altera a flag para a thread parar
+        running = false;
+        Log.d(TAG, "ExemploServico.onDestroy()");
+    }
+}
