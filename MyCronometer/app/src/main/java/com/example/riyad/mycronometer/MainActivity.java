9e16b2e3408db70d752f7102056800b7d0072d90
RiyadhSilva <riyadhlevi@gmail.com>
2016-09-22 20:45:04 -0300

    Implementação - ImageView e Button
    Implementação de uma ImageView para ser exibida assim que o button
    bt_Iniciar for acionado. Implementação também do button bt_Parar e seus
    respectivos métodos para finalizar o cronômetro.

--- MyCronometer/app/src/main/java/com/example/riyad/mycronometer/MainActivity.java
+++ MyCronometer/app/src/main/java/com/example/riyad/mycronometer/MainActivity.java
@@ -4,19 +4,41 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public ImageView imageView;
+    public Button bt_parar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startService(new Intent(this, HelloIntentService.class));
+        imageView = (ImageView) findViewById(R.id.imageView);
+        bt_parar = (Button) findViewById(R.id.bt_Parar);
+
 
     }
 
     public void inicia (View view){
+        imageView.setVisibility(View.VISIBLE);
+        bt_parar.setVisibility(View.VISIBLE);
         startService(new Intent(this, HelloIntentService.class));
+        toast("Cronômetro iniciado!");
+    }
+
+    public void encerra(View view){
+        imageView.setVisibility(View.INVISIBLE);
+        bt_parar.setVisibility(View.INVISIBLE);
+        stopService(new Intent(this, HelloIntentService.class));
+        toast("Cronômetro encerrado!");
+    }
+
+    private void toast(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+
     }
 }
